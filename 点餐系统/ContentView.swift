bb7df0df3cb7cd2413e80cf25346f90a7cf6d302
Your Name <your.email@example.com>
2025-03-19 15:13:36 -0400

    Initial Commit

---
+++ 点餐系统/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  点餐系统
+//
+//  Created by Zhe Cheng on 2025-03-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
